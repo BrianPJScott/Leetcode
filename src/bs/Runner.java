0f38d1c64335038ba99698926eed9f5392fcd6f6
Brian Scott <brianpjscott@gmail.com>
2024-09-11 21:54:10 +0100

    Leetcode27

--- src/bs/Runner.java
+++ src/bs/Runner.java
@@ -1,10 +1,25 @@
 package bs;
 
+import java.util.Arrays;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 
+		Leet27 leet = new Leet27();
+		int[] myArray = {3,2,2,3 };
+		int i = leet.removeElement(myArray, 3);
+		
+		for (int e : myArray) System.out.print(e);
+		System.out.println();
+		System.out.println(i);
+
+		int[] myArray2 = { 0, 1, 2, 2, 3, 0, 4, 2 };
+		i = leet.removeElement(myArray2, 2);
+		
+		for (int e : myArray2) System.out.print(e);
+		System.out.println();
+		System.out.println(i);
 	}
 
 }
